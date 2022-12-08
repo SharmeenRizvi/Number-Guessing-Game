068a7562f53936ff072559f209fde114596cca33
Syeda Sharmeen Fatima <101641319+SharmeenRizvi@users.noreply.github.com>
2022-12-08 16:56:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let randomNum = Math.floor(Math.random() * 10);
+const guessNumFunc = async () => {
+    for (let i = 0; i < 4; i++) {
+        let guessNum = await inquirer.prompt([
+            {
+                name: "num",
+                type: "number",
+                message: "Guess the number",
+            },
+        ]);
+        if (guessNum.num === randomNum) {
+            console.log(chalk.bgMagenta("You guessed it!"));
+            break;
+        }
+        else {
+            console.log(chalk.bgRed("Try Again"));
+        }
+    }
+    console.log(chalk.bgBlue("The number was '" + randomNum + "'"));
+};
+guessNumFunc();
